b7e32fbc37af347946d51361b499048dac4ae168
Farouk <faroukkisuule55@gmail.com>
2021-05-07 06:46:17 -0400

    Express Server has been connected and all packages installed

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded( { extended : true } ));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, (req,res)=>{
+    console.log("Listening to Port " + PORT)
+});
